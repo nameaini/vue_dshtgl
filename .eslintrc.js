38684e742b55e5dafb58cdf978f72940b9342d54
小猫 <y1270987411@163.com>
2019-11-24 13:09:44 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,12 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    // '@vue/standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren':0
   },
   parserOptions: {
     parser: 'babel-eslint'
