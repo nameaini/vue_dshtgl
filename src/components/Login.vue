81d54812a72d6749e89cea4f406ec63ebbbdc1fc
小猫 <y1270987411@163.com>
2019-11-25 19:29:52 +0800

    完成用户列表功能的开发

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -26,21 +26,21 @@
 </template>
 
 <script>
-export default {
+  export default {
     data() {
         return {
-            //这是登录表单的数据绑定对象
+            // 这是登录表单的数据绑定对象
             loginForm: {
                 username: 'admin',
                 password: '123456'
             },
-            //这是表单的验证规则对象
+            // 这是表单的验证规则对象
             loginFormRules: {
-                //验证用户名是否合法
+                // 验证用户名是否合法
                 username: [ { required: true, message: '请输入登录名称', trigger: 'blur' },
                 { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
                 ],
-                //验证密码是否合法
+                // 验证密码是否合法
                 password: [
                      { required: true, message: '请输入登录密码', trigger: 'blur' },
                 { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
@@ -49,7 +49,7 @@ export default {
         }
     },
     methods: {
-        //点击重置按钮，重置登录表单
+        // 点击重置按钮，重置登录表单
         resetLoginForm() {
             // console.log(this);
             this.$refs.loginFormRef.resetFields();
@@ -60,12 +60,12 @@ export default {
                 const {data:res} = await this.$http.post('login',this.loginForm);
                 if(res.meta.status !==200) return this.$message.error('登录失败');
                 this.$message.success('登录成功');
-                //1,将登录成功之后的token，保存到客户端的 sessionStorage中
-                //1.1项目中除了登录之外的其他API接口，必须在登录之后才能访问
-                //1.2token只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+                // 1,将登录成功之后的token，保存到客户端的 sessionStorage中
+                // 1.1项目中除了登录之外的其他API接口，必须在登录之后才能访问
+                // 1.2token只应在当前网站打开期间生效，所以将token保存在sessionStorage中
                 console.log(res);
                 window.sessionStorage.setItem('token',res.data.token);
-                //2.通过编程式导航跳转到后台主页，路由地址是/home
+                // 2.通过编程式导航跳转到后台主页，路由地址是/home
                 this.$router.push('/home')
             });
         }
@@ -75,7 +75,7 @@ export default {
 
 <style lang="less" scoped>
 .login_container{
-    background-color: yellowgreen;
+    background-color: #7dadaf;
     height:100%;
  }
 .login_box{
@@ -83,7 +83,7 @@ export default {
      height: 300px;
      background-color: #14bf75;
      border-radius: 3px;
-     position: absolute;            
+     position: absolute;           
      left: 50%;
      top: 50%;
      transform:translate(-50%,-50%);
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/User/Users.vue'
 
 Vue.use(Router)
 
@@ -9,21 +11,27 @@ const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/Welcome',
+      children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: Users }
+      ]
+    }
   ]
 })
 
-//挂载路由导航守卫
+// 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-  //to将要访问的路径
-  //from代表从哪个路径跳转而来
-  //next是一个函数，表示放行
+  // to将要访问的路径
+  //  from代表从哪个路径跳转而来
+  //  next是一个函数，表示放行
   //   next() 放行  next('/login) 强制跳转
 
-  if(to.path === '/login') return next();
-  //获取token
+  if (to.path === '/login') return next()
+  //  获取token
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 export default router
