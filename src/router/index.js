857958656bf3691d21b62893ad4e238386748f96
小猫 <y1270987411@163.com>
2019-11-26 22:46:24 +0800

    完成了权限功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/User/Users.vue'
+import Rights from '../components/power/Rights.vue'
+import Roles from  '../components/power/Roles.vue'
 
 Vue.use(Router)
 
@@ -15,7 +17,9 @@ const router = new Router({
       path: '/home',
       component: Home,
       redirect: '/Welcome',
-      children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: Users }
+      children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: Users },
+      { path: '/rights' , component: Rights },
+      { path: '/roles' , component: Roles}
       ]
     }
   ]
